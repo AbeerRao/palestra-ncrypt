e20ed490c04584de37fa2e99d34f0de693f90159
AbeerRao <abeerrao@icloud.com>
2024-05-10 15:09:18 +0530

    login for laptop

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,16 +7,16 @@ import LoginImg from "../images/LoginImg.svg"
 function Login() {
 
     const styles = {
-        main: "flex flex-row items-center w-screen h-screen justify-center p-32 space-x-10",
+        main: "flex flex-row items-center w-screen h-screen justify-center 2xl:p-32 p-24 space-x-10",
         leftDiv: "flex flex-col h-full w-1/2 justify-between items-start",
-        topDiv: "flex flex-row items-center justify-between w-1/2",
+        topDiv: "flex flex-row items-center justify-between 2xl:w-1/2 w-2/3",
         textDiv: "flex flex-col items-start space-y-2",
         title: "text-6xl font-semibold text-center",
         desc: "font-light",
         form: "flex flex-col items-start space-y-8",
         input: "bg-transparent border-b-2 border-black w-full p-2 text-black placeholder-black text-xl focus:outline-none focus:border-accent transition duration-300",
         button: "w-full bg-transparent text-xl font-semibold py-2 rounded-full border-2 border-black transition duration-300 disabled:bg-gray-200 disabled:border-gray-200 disabled:text-gray-500 disabled:cursor-not-allowed disabled:font-normal hover:bg-black hover:text-accent hover:scale-95",
-        bottomText: "text-sm font-light text-center w-1/2",
+        bottomText: "text-sm font-light 2xl:text-center w-1/2",
         rightDiv: "w-1/2 h-max",
         image: "w-full h-full object-cover"
     }
